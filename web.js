b0e80d6a1223ac63921c56bafc91f383cd417f0c
Vivek K S <ksvivek@gmail.com>
2013-07-24 19:23:00 +0530

    Adding marketing copy and initial images

--- web.js
+++ web.js
@@ -4,10 +4,19 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html').toString());
+    console.log("Got a GET request for /");
+    response.send(fs.readFileSync('index.html').toString());
 });
 
+app.get(/^(.+)$/, function(request, response) {
+    console.log("Got a GET request for " + request.params[0]);
+    response.header('Content-Type','image/png');
+    response.send(fs.readFileSync(request.params[0].slice(1)));
+});
+
+
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
